65e13cfbf421bd7f313ce6b3032b23813d2b5ac8
sharpiro <dsharpbb09@gmail.com>
2016-07-02 16:08:22 -0400

    Initial Commit

---
+++ app/DSharpCompiler.Web/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace DSharpCompiler.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder().UseKestrel().UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration().UseStartup<Startup>().Build().Run();
+        }
+    }
+}
