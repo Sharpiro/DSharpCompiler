f35b3614cdba5199f447d291f30a8a32a5f1cac0
David Sharp <david.m.sharp@wellsfargo.com>
2016-07-05 12:42:22 -0400

    Part 4, 5, 6 Precedence, Deep nested parenthesis

---
+++ src/DSharpCompiler.Core/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSharpCompiler.Core
+{
+    public static class Extensions
+    {
+        public static bool ContainsAny<T>(this IEnumerable<T> list, params T[] otherList)
+        {
+            foreach (var item in list)
+            {
+                foreach (var item2 in otherList)
+                {
+                    if (item.Equals(item2))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool In<T>(this string value, params T[] list)
+        {
+            foreach (var item in list)
+            {
+                if (item.Equals(value))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
--- test/DSharpCompiler.Core.Tests/PartFiveAndSixTests.cs
+++ test/DSharpCompiler.Core.Tests/PartFiveAndSixTests.cs
@@ -1,53 +1,41 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Xunit;
 
 namespace DSharpCompiler.Core.Tests
 {
-    public class PartOneTests
+    public class PartFiveAndSixTests
     {
         [Fact]
-        public void BasicAdditionTest()
+        public void PrecedenceTest()
         {
-            var code = "3+5";
+            var code = "14 + 2 * 3 - 6 / 2";
             var lexer = new LexicalAnalyzer(code);
             var tokens = lexer.Analayze();
             var parser = new TokenParser(tokens.ToList());
             var result = parser.Expression();
-            Assert.Equal(8, result);
+            Assert.Equal(17, result);
         }
 
         [Fact]
-        public void MultiDigitTest()
+        public void ShallowNestingTest()
         {
-            var code = "12+3";
+            var code = "2 * (7 + 3)";
             var lexer = new LexicalAnalyzer(code);
             var tokens = lexer.Analayze();
             var parser = new TokenParser(tokens.ToList());
             var result = parser.Expression();
-            Assert.Equal(15, result);
+            Assert.Equal(20, result);
         }
 
         [Fact]
-        public void WhiteSpaceTest()
+        public void DeepNestingTest()
         {
-            var code = " 12 + 3";
+            var code = "7 + 3 * (10 / (12 / (3 + 1) - 1))";
             var lexer = new LexicalAnalyzer(code);
             var tokens = lexer.Analayze();
             var parser = new TokenParser(tokens.ToList());
             var result = parser.Expression();
-            Assert.Equal(15, result);
-        }
-
-        [Fact]
-        public void SubtractionTest()
-        {
-            var code = "7-5";
-            var lexer = new LexicalAnalyzer(code);
-            var tokens = lexer.Analayze();
-            var parser = new TokenParser(tokens.ToList());
-            var result = parser.Expression();
-            Assert.Equal(2, result);
+            Assert.Equal(22, result);
         }
     }
 }
