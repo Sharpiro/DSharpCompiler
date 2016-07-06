f3644171b67bc32f5c971356ada3c536ee5de945
sharpiro <dsharpbb09@gmail.com>
2016-07-06 16:00:58 -0400

    starting on part 9 lexical peek added

---
+++ test/DSharpCompiler.Core.Tests/PartNineTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace DSharpCompiler.Core.Tests
+{
+    public class PartNineTests
+    {
+        [Fact]
+        public void PeekTest()
+        {
+            var code = "BEGIN a := 2; END.";
+            var lexer = new LexicalAnalyzer(code);
+            var tokens = lexer.Analayze();
+            Assert.Equal(7, tokens.Count());
+            //var parser = new TokenParser(tokens.ToList());
+            //var rootNode = parser.Expression();
+            //var interpreter = new Interpreter(rootNode);
+            //var result = interpreter.Interpret();
+        }
+    }
+}
