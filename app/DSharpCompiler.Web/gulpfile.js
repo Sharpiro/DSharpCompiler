f1df01ed36fa7ea357905c8a14f9664843446f03
sharpiro <dsharpbb09@gmail.com>
2016-07-08 11:17:41 -0400

    updating gulpfile to a more manual approach for azure

--- app/DSharpCompiler.Web/gulpfile.js
+++ app/DSharpCompiler.Web/gulpfile.js
@@ -1,20 +1,39 @@
-﻿var gulp = require('gulp');
+﻿
+var path = require('path');
+var gulp = require('gulp');
 
-gulp.task("copyNodeModules", () => {
-    gulp.src([
-            'core-js/client/**',
-            'systemjs/dist/**',
-            'reflect-metadata/**',
-            'rxjs/**',
-            'zone.js/dist/**',
-            '@angular/**',
-            'jquery/dist/jquery.*js',
-            'bootstrap/dist/**',
-            "toastr/build/**"
-    ], {
-        cwd: "node_modules/**"
-    })
-        .pipe(gulp.dest("./wwwroot/libs"));
+var webroot = "./wwwroot/";
+
+var config = {
+    libBase: 'node_modules',
+    lib: [
+        require.resolve('@angular/common/bundles/common.umd.js'),
+        require.resolve('@angular/common/bundles/common.umd.js'),
+        require.resolve('@angular/compiler/bundles/compiler.umd.js'),
+        require.resolve('@angular/core/bundles/core.umd.js'),
+        require.resolve('@angular/forms/bundles/forms.umd.js'),
+        require.resolve('@angular/http/bundles/http.umd.js'),
+        require.resolve('@angular/platform-browser/bundles/platform-browser.umd.js'),
+        require.resolve('@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js'),
+        require.resolve('@angular/router-deprecated/bundles/router-deprecated.umd.js'),
+        require.resolve('@angular/upgrade/bundles/upgrade.umd.js'),
+        require.resolve('bootstrap/dist/css/bootstrap.min.css'),
+        require.resolve('bootstrap/dist/js/bootstrap.min.js'),
+        path.dirname(require.resolve('bootstrap/dist/fonts/glyphicons-halflings-regular.woff')) + '/**',
+        require.resolve('core-js/client/shim.min.js'),
+        require.resolve('jquery/dist/jquery.min.js'),
+        require.resolve('reflect-metadata/Reflect.js'),
+        path.dirname(require.resolve('rxjs/Rx.js')) + '/**',
+        require.resolve('systemjs/dist/system.src.js'),
+        require.resolve('toastr/build/toastr.min.js'),
+        require.resolve('toastr/build/toastr.min.css'),
+        require.resolve('zone.js/dist/zone.js'),
+    ]
+};
+
+gulp.task('copyModules', function () {
+    return gulp.src(config.lib, { base: config.libBase })
+        .pipe(gulp.dest(webroot + 'lib'));
 });
 
-gulp.task('default', ['copyNodeModules']);
+gulp.task("default", ["copyModules"])
--- app/DSharpCompiler.Web/wwwroot/systemjs.config.js
+++ app/DSharpCompiler.Web/wwwroot/systemjs.config.js
@@ -6,9 +6,9 @@
     // map tells the System loader where to look for things
     var map = {
         'app': 'app', // 'dist',
-        '@angular': 'libs/@angular',
+        '@angular': 'lib/@angular',
         'angular2-in-memory-web-api': 'libs/angular2-in-memory-web-api',
-        'rxjs': 'libs/rxjs'
+        'rxjs': 'lib/rxjs'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
