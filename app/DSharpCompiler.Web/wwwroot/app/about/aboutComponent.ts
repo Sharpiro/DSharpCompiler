5c80119bc919bffa943b8711af2208cf601f7d11
Sharpiro <dsharpbb09@gmail.com>
2016-07-08 14:26:18 -0400

    Feature/web init (#2)
    * starting on web interface with angular 2
    
    * functional site
    trying to get to publish correctly
    
    * updated typings and post publish preferences
    
    * changed to prepublish
    
    * publish now working locally
    
    * updating gulpfile to a more manual approach for azure
    
    * added gulp typescript to transpile src in azure
    
    * added about page

---
+++ app/DSharpCompiler.Web/wwwroot/app/about/aboutComponent.ts
@@ -0,0 +1,11 @@
+﻿import {Component} from "@angular/core"
+
+@Component({
+    selector: "appComponent",
+    templateUrl: "./app/about/aboutComponent.html",
+    styleUrls: ["./app/about/aboutComponent.css"]
+})
+export class AboutComponent
+{
+
+}
---
+++ app/DSharpCompiler.Web/wwwroot/app/appComponent.ts
@@ -0,0 +1,61 @@
+﻿import {Component, provide, OpaqueToken} from "@angular/core"
+import {Location} from "@angular/common"
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from "@angular/router-deprecated"
+import {InMemoryBackendService, SEED_DATA}  from "angular2-in-memory-web-api/";
+import {InMemoryDb} from "../fakeApi/inMemoryDb"
+import {HTTP_PROVIDERS, XHRBackend} from "@angular/http"
+import {DashboardComponent, StaticVehicleService, VehicleService, VehiclesComponent,
+    VehicleListComponent, VehicleComponent, SpinnerComponent, SpinnerService, AboutComponent} from "./appCore"
+
+@Component({
+    selector: "my-app",
+    directives: [ROUTER_DIRECTIVES, DashboardComponent, SpinnerComponent],
+    providers: [HTTP_PROVIDERS, provide("IVehicleServiceToken", { useClass: VehicleService }),
+        //provide(XHRBackend, { useClass: InMemoryBackendService }),
+        //provide(SEED_DATA, { useClass: InMemoryDb }),
+        SpinnerService],
+    templateUrl: "./app/appComponent.html",
+    styles: [
+        `nav ul {list-style-type: none;}
+        nav ul li {padding: 4px;display:inline-block}
+        bg-color { background-color: #ECECEA }
+`
+    ]
+})
+@RouteConfig([
+    { path: "/dashboard", name: "Dashboard", component: DashboardComponent, useAsDefault: true },
+    { path: "/about", name: "About", component: AboutComponent },
+    { path: "/vehicles/...", name: "Vehicles", component: VehiclesComponent }
+])
+export class AppComponent
+{
+    public showNavValue = "";
+
+
+    constructor(private _location: Location)
+    {
+
+    }
+
+    public changed(changedCharacter: IBaseData): void
+    {
+        const message = `Event changed: ${changedCharacter.name}`;
+        toastr.success(message);
+        console.log(message);
+    }
+
+    private isActive(path: string): string
+    {
+        const currentPath = this._location.path();
+        const result = currentPath === path ? "active" : null;
+        return result;
+    }
+
+    private toggleNav(): void
+    {
+        if (this.showNavValue === "in")
+            this.showNavValue = "";
+        else
+            this.showNavValue = "in";
+    }
+}
